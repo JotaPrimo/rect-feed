e0641c82fb7f5c64bd73fd3736df7ccda91db310
JotaPrimo <gestald118@gmail.com>
2023-01-28 16:39:29 -0400

    propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ function App() {
 
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />  
+      <Post 
+        author="Diego fernandes" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
+      />
+      <Post 
+        author="Jota Santos" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
+      />      
     </div>
   )
 }
 
 export default App
+
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,8 @@
-export function Post() {
-    return <p>Post Component</p>
+export function Post(props) {
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  );
 }
