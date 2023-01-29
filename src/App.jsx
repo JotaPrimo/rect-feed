b9f2964a64e3bf3479dd641619ae8680a5edd71e
JotaPrimo <gestald118@gmail.com>
2023-01-29 12:44:21 -0400

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import './global.css';
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
-
   return (
     <div>
-
       <Header />
-      <Post 
-        author="Diego fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
-      />
-      <Post 
-        author="Jota Santos" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
-      />      
+
+      <div className={styles.wrapper}>
+       <Sidebar />
+        <main>
+          <Post
+            author="Diego fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
+          />
+          <Post
+            author="Jota Santos"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
+          />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
