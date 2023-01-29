06db7767b2e425fd7923a3acde0905aeb8fdcb35
JotaPrimo <gestald118@gmail.com>
2023-01-29 14:42:22 -0400

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,12 +9,33 @@ export function Sidebar() {
         alt=""
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/63259982?v=4"
+        />
         <strong>Diego Fernandes</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-edit-3"
+          >
+            <path d="M12 20h9"></path>
+            <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+          </svg>
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
