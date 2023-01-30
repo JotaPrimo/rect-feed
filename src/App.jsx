35d4b8961301bfa4c33be1a92ff2bb670c98f685
sistemasIMCG <sistemas@institutomirim.org.br>
2023-01-30 12:51:25 -0400

    Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,52 @@ import styles from "./App.module.css";
 import "./global.css";
 import { Sidebar } from "./components/Sidebar";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/JotaPrimo.png',
+      name: 'Jota Santos',
+      role: 'Dev Web',
+    }, 
+    content: [
+      { type: 'pharagraph'},
+      { type: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using' },
+      { type: 'link', content: 'gestald118@gmail.com'}
+    ],
+    publishedAt: new Date('2022-05-10 20:00:54')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/JotaPrimo.png',
+      name: 'Jota Santos',
+      role: 'Dev Web',
+    }, 
+    content: [
+      { type: 'pharagraph'},
+      { type: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using' },
+      { type: 'link', content: 'jessicaHelena@gmail.com'}
+    ],
+    publishedAt: new Date('2022-05-10 20:10:36')
+  }
+];
+
 function App() {
   return (
     <div>
       <Header />
 
       <div className={styles.wrapper}>
-       <Sidebar />
+        <Sidebar />
         <main>
-          <Post
-            author="Diego fernandes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
-          />
-          <Post
-            author="Jota Santos"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aitae atque cupiditate, maxime, ut obcaecati veritatis deleniti et."
-          />
+          { posts.map(post => {
+            return <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />;
+          }) }
         </main>
       </div>
     </div>
